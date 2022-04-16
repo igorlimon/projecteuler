99430c9ca0a6e0008591d4e2cc589538de896461
Igor Jigan <Igor.Jigan@ing.com>
2022-04-16 22:23:10 +0300

    Solved problem 7

---
+++ Problem7/Program.cs
@@ -0,0 +1,18 @@
+﻿var primeNumbers = new List<long>() {2,3,5,7,11,13};
+var i = 15;
+while(primeNumbers.Count <= 10001){
+	bool isPrime = true;
+	foreach(var n in primeNumbers){
+		if(i % n == 0 ){
+			isPrime = false;
+			break;
+		} else if(n*2 >=i){
+			break;
+		}
+	}
+	if(isPrime){
+		primeNumbers.Add(i);
+	}
+	i+=2;
+}	
+Console.WriteLine(primeNumbers[10001]);	
