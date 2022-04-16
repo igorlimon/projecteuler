28634c90ff0c55e967102c48c811ff0b9eed00b5
Igor Jigan <Igor.Jigan@ing.com>
2022-04-16 22:09:07 +0300

    Added implementation for Problem9

---
+++ Problem9/Program.cs
@@ -0,0 +1,17 @@
+﻿int a, b, c;
+for (a = 1000; a >= 1; a--)
+{
+    for (b = a-1; b >= 1; b--)
+    {
+        c = 1000 - a - b;
+        if (a * a + b * b == c * c)
+        
+            if (a + b + c == 1000)
+            {
+                Console.WriteLine(a + " " + b + " " + c);
+                Console.WriteLine(a * b * c);
+                break;
+            }
+        }
+    }
+}
